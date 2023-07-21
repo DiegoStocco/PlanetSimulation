726ce88ce040835d7cd74c4a07eed97429751a43
DiegoStocco <diegostocco.2008@gmail.com>
2023-07-21 17:22:07 +0200

    Finished basic implementation - Working release

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 #include "vec2.h"
 #include "planet.h"
 
-SDL_Renderer* renderer = NULL;
 SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
 
 void init(const int SCREEN_WIDTH, const int SCREEN_HEIGHT)
 {
   SDL_Init(SDL_INIT_VIDEO);
-  window = SDL_CreateWindow("Pianeti", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SDL_WINDOW_SHOWN);
+  window = SDL_CreateWindow("Pianeti", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC|SDL_RENDERER_ACCELERATED);
 }
 
@@ -25,15 +25,48 @@ void quit()
   SDL_Quit();
 }
 
-int main (int argc, char *argv[]) {
-  const int SCREEN_WIDTH = 500;
-  const int SCREEN_HEIGHT = 500;
+int main(int argc, char *argv[])
+{
+  const int SCREEN_WIDTH = 400;
+  const int SCREEN_HEIGHT = 400;
   init(SCREEN_WIDTH, SCREEN_HEIGHT);
   
   std::vector<Planet> planets = {
-    Planet(5.9726e24, vec2(0, 1.496e11), vec2(2.978e4, 0)),  // Earth
-    Planet(1.989e30, vec2(0, 0), vec2(0, 0))                 // Sun
+    Planet("Sun", 1.989e30, 20, 0xffdc13ff, vec2(0, 0), vec2(0, 0)),
+    Planet("Mercury", 3.301e23, 5, 0xfff5d5ff, vec2(0, 5.791e10), vec2(3.886e4, 0)),
+    Planet("Venus", 4.867e24, 7, 0xfff5aaff, vec2(0, 1.082e11), vec2(3.479e4, 0)),
+    Planet("Earth", 5.9726e24, 15, 0x67ff67ff, vec2(0, 1.496e11), vec2(2.929e4, 0)),
+    Planet("Mars", 6.417e23, 12, 0xff6f6fff, vec2(0, 2.279e11), vec2(2.1972e4, 0))
   };
 
+  const double scale = 1e9;
+  const vec2 top_left_corner = vec2(-SCREEN_WIDTH/2.0, -SCREEN_HEIGHT/2.0);
+  const double delta_t = 10000;
+
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+  
+  bool running = true;
+  SDL_Event event;
+  while(running)
+  {
+    while(SDL_PollEvent(&event))
+    {
+      if(event.type == SDL_QUIT) running = false;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    for(int i = 0; i < planets.size(); i++)
+    {
+      planets[i].update(delta_t, &planets);
+      planets[i].render(renderer, scale, top_left_corner);
+    }
+    SDL_RenderPresent(renderer);
+
+  }
+
+  quit();
   return 0;
 }
--- planet.h
+++ planet.h
@@ -3,47 +3,53 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
-#include <SDL2/SDL_stdinc.h>
 #include <vector>
+#include <string>
 #include "vec2.h"
 #include "physical_constants.h"
 
 class Planet
 {
   public:
-    Planet(double massas, vec2 initial_position, vec2 initial_velocity)
-    {
-      mass = massas;
-      position = initial_position;
-      velocity = initial_velocity;
-    }
+    Planet(std::string name, double mass, double radius, Uint32 color, vec2 initial_position, vec2 initial_velocity):
+      name(name),
+      mass(mass),
+      radius(radius),
+      color(color),
+      position(initial_position),
+      velocity(initial_velocity)
+  {};
 
-    void update(double delta_t, std::vector<Planet> planets)
+    void update(double delta_t, std::vector<Planet>* planets)
     {
       acceleration = vec2(0,0);
       vec2 distance;
-      for(Planet planet : planets)
+      for(Planet planet : *planets)
       {
-        distance = position - planet.position;
-        acceleration += ((G * mass * planet.mass) / distance.length_squared()) * unit_vector(distance);
+        if(planet.name != name)
+        {
+          distance = planet.position - position;
+          acceleration += ((G * planet.mass) / distance.length_squared()) * unit_vector(distance);
+        }
       }
-      velocity += acceleration * delta_t;
-      position += velocity * delta_t;
+      velocity += (acceleration * delta_t);
+      position += (velocity * delta_t);
     }
 
     void render(SDL_Renderer* renderer, double scale, vec2 up_left_corner = vec2(0,0))
     {
       vec2 display_position = position / scale;
-      display_position += up_left_corner;
-      double display_radius = radius / scale;
+      display_position += -up_left_corner;
 
-      aacircleColor(renderer, display_position.x(), display_position.y(), display_radius, color);
+      filledCircleColor(renderer, display_position.x(), display_position.y(), radius, color);
+      aacircleColor(renderer, display_position.x(), display_position.y(), radius, color);
     }
   
   public:
+    std::string name;
     double mass;
     double radius;
-    Uint32 color = 0xff0000ff;
+    Uint32 color = 0xffffffff;
     vec2 position;
     vec2 velocity;
     vec2 acceleration;
