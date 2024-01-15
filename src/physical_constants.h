3fabb1cd76449f619142f3465da22bda6a1e51d5
DiegoStocco <diegostocco.2008@gmail.com>
2024-01-15 19:10:06 +0100

    Changed constant type to constexpr

--- src/physical_constants.h
+++ src/physical_constants.h
@@ -1,54 +1,54 @@
 #ifndef PHYSYCAL_CONSTANTS_H
 #define PHYSYCAL_CONSTANTS_H
 
-const double G = 6.674e-11;
+constexpr double G = 6.674e-11;
 
-const double SUN_MASS = 1.9885e30;
-const double SUN_RADIUS = 6.96342e8;
+constexpr double SUN_MASS = 1.9885e30;
+constexpr double SUN_RADIUS = 6.96342e8;
 
-const double MERCURY_MASS = 3.30103e23;
-const double MERCURY_RADIUS = 2.4394e6;
-const double MERCURY_MIN_SPEED = 3.8860e4;
-const double SUN_MERCURY_MAX_D = 6.982e10;
+constexpr double MERCURY_MASS = 3.30103e23;
+constexpr double MERCURY_RADIUS = 2.4394e6;
+constexpr double MERCURY_MIN_SPEED = 3.8860e4;
+constexpr double SUN_MERCURY_MAX_D = 6.982e10;
 
-const double VENUS_MASS = 4.86731e24;
-const double VENUS_RADIUS = 6.0518e6;
-const double VENUS_MIN_SPEED = 3.476e4;
-const double SUN_VENUS_MAX_D = 1.0894e11;
+constexpr double VENUS_MASS = 4.86731e24;
+constexpr double VENUS_RADIUS = 6.0518e6;
+constexpr double VENUS_MIN_SPEED = 3.476e4;
+constexpr double SUN_VENUS_MAX_D = 1.0894e11;
 
-const double EARTH_MASS = 5.97217e24;
-const double EARTH_RADIUS = 6.3710084e6;
-const double EARTH_MIN_SPEED = 2.9291e4;
-const double SUN_EARTH_MAX_D = 1.52097597e11;
+constexpr double EARTH_MASS = 5.97217e24;
+constexpr double EARTH_RADIUS = 6.3710084e6;
+constexpr double EARTH_MIN_SPEED = 2.9291e4;
+constexpr double SUN_EARTH_MAX_D = 1.52097597e11;
 
-const double MOON_MASS = 7.342e22;
-const double MOON_RADIUS =  1.738e6;
-const double MOON_MIN_SPEED = 9.64e2;
-const double EARTH_MOON_MAX_D = 4.055e8;
+constexpr double MOON_MASS = 7.342e22;
+constexpr double MOON_RADIUS =  1.738e6;
+constexpr double MOON_MIN_SPEED = 9.64e2;
+constexpr double EARTH_MOON_MAX_D = 4.055e8;
 
-const double MARS_MASS = 6.41691e23;
-const double MARS_RADIS = 3.38950e6;
-const double MARS_MIN_SPEED = 2.1972e4;
-const double SUN_MARS_MAX_D = 2.49261e11;
+constexpr double MARS_MASS = 6.41691e23;
+constexpr double MARS_RADIS = 3.38950e6;
+constexpr double MARS_MIN_SPEED = 2.1972e4;
+constexpr double SUN_MARS_MAX_D = 2.49261e11;
 
-const double JUPITER_MASS = 1.898125e27;
-const double JUPITER_RADIUS = 6.9911e7;
-const double JUPITER_MIN_SPEED = 1.2446e4;
-const double SUN_JUPITER_MAX_D = 8.16363e11;
+constexpr double JUPITER_MASS = 1.898125e27;
+constexpr double JUPITER_RADIUS = 6.9911e7;
+constexpr double JUPITER_MIN_SPEED = 1.2446e4;
+constexpr double SUN_JUPITER_MAX_D = 8.16363e11;
 
-const double SATURN_MASS = 5.68317e26;
-const double SATURN_RADIUS = 5.8232e4;
-const double SATURN_MIN_SPEED = 9.09e3;
-const double SUN_SATURN_MAX_D = 1.51450e12;
+constexpr double SATURN_MASS = 5.68317e26;
+constexpr double SATURN_RADIUS = 5.8232e4;
+constexpr double SATURN_MIN_SPEED = 9.09e3;
+constexpr double SUN_SATURN_MAX_D = 1.51450e12;
 
-const double URANUS_MASS = 8.68099e25;
-const double URANUS_RADIUS = 2.5362e4;
-const double URANUS_MIN_SPEED = 6.49e3;
-const double SUN_URANUS_MAX_D = 3.00639e12;
+constexpr double URANUS_MASS = 8.68099e25;
+constexpr double URANUS_RADIUS = 2.5362e4;
+constexpr double URANUS_MIN_SPEED = 6.49e3;
+constexpr double SUN_URANUS_MAX_D = 3.00639e12;
 
-const double NEPTUNE_MASS = 1.024092e26;
-const double NEPTUNE_RADIUS = 2.4622e4;
-const double NEPTUNE_MIN_SPEED = 5.385e3;
-const double SUN_NEPTUNE_MAX_D = 4.558857e12;
+constexpr double NEPTUNE_MASS = 1.024092e26;
+constexpr double NEPTUNE_RADIUS = 2.4622e4;
+constexpr double NEPTUNE_MIN_SPEED = 5.385e3;
+constexpr double SUN_NEPTUNE_MAX_D = 4.558857e12;
 
 #endif
